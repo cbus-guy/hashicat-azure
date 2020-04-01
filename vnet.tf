ad64a1e8f180abddb478d17fc00a8ce6a9f52166
Kevin C <kevin101@hotmail.com>
2020-04-01 19:04:23 +0000

    Module example4

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,16 @@
-resource "azurerm_resource_group" "gaurav" {
-  name     = "gaurav"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "gaurev" {
+  name     = "my-resources"
   location = "West Europe"
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.gaurav.name
-  address_space       = "10.0.0.0/16"
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.gaurev.name
+  address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
